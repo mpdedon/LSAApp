892fc1ef97177c1e3b76573d597e003029be98a2
Mpdedon <umarsmukhtar@gmail.com>
2025-02-15 16:17:40 +0100

    Postgres Database for Render

--- lsaapp/settings.py
+++ lsaapp/settings.py
@@ -12,23 +12,22 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+import dj_database_url
+
+# Load environment variables from .env file
+load_dotenv()  
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kf52q09*k_sqh%1lda&b8*msq*jg&@yk=n!#!ejrf1&3=#**&q'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = []
-
-
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*,localhost:8000,127.0.0.1:8000,0.0.0.0:8000,learnswift.icu').split(',')
 # Application definition
 
 INSTALLED_APPS = [
@@ -56,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'lsaapp.urls'
@@ -82,13 +82,26 @@ WSGI_APPLICATION = 'lsaapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASE_URL = os.getenv("DATABASE_URL")  # Fetch database URL from environment variables
 
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'lsaapp', 
+            'USER': 'lsaapp_user', 
+            'PASSWORD': 'password',  
+            'HOST': 'localhost',
+            'PORT': '5432',
+            'OPTIONS': {
+            'options': '-c search_path=lsaapp_schema'
+        },
+        }
+    }
 
 AUTH_USER_MODEL = 'core.CustomUser'
 
@@ -125,7 +138,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'core/static')
     ]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Media files
 
 MEDIA_URL = '/media/'
@@ -145,59 +158,37 @@ SECURE_HSTS_PRELOAD = False
 SECURE_HSTS_INCLUDE_SUBDOMAINS = False
 
 CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000'
     'https://learnswift.icu',  # Add your domain here
 ]
 
-
 # Set a session timeout (optional)
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+LOG_DIR = '/app/logs'
+if not os.path.exists(LOG_DIR):
+    os.makedirs(LOG_DIR)
 
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
         'console': {
-            'level': 'INFO',
+            'level': 'ERROR',
             'class': 'logging.StreamHandler',
         },
         'file': {
             'level': 'ERROR',
             'class': 'logging.FileHandler',
-            'filename': 'logs/error.log',
+            'filename': os.path.join(LOG_DIR, 'error.log'),
+            'mode': 'a',  # Append mode
         },
     },
     'loggers': {
         'django': {
-            'handlers': ['console'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-        'custom': {
-            'handlers': ['file'],
+            'handlers': ['file', 'console'],  # Log to both file and console
             'level': 'ERROR',
-            'propagate': False,
+            'propagate': True,
         },
     },
 }
-
-# Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mailgun.org'  # Replace with your SMTP host
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'postmaster@sandbox9e3dcf230bfb4f3d93a83515c6f1ec97.mailgun.org'
-EMAIL_HOST_PASSWORD = 'befb6f71f7702b43fe94ef6cc5a0a6d3-78f6ccbe-1ddbc4fe'
-DEFAULT_FROM_EMAIL = 'LSAApp <postmaster@sandbox9e3dcf230bfb4f3d93a83515c6f1ec97.mailgun.org>'
-
-# Optional: Configure AnyMail for transactional email services
-INSTALLED_APPS += ['anymail']
-ANYMAIL = {
-    "MAILGUN_API_KEY": "your-mailgun-api-key",
-    "MAILGUN_SENDER_DOMAIN": "your-domain.com",
-}
-
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-
-
