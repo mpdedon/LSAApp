171315d64b7cf76026c126da6e5d2adc3ecabd14
Mpdedon <umarsmukhtar@gmail.com>
2025-04-06 01:28:59 +0100

    Migration issues

--- lsaapp/settings.py
+++ lsaapp/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*,localhost:8001,127.0.0
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
     'crispy_forms',
     'crispy_bootstrap5',
     'django.contrib.humanize',
