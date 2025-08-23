23365e0633ed04848c5f6011936c43cbab7b6199
Mpdedon <umarsmukhtar@gmail.com>
2025-08-23 10:57:14 +0100

    Template Syntax Resolution

--- core/templatetags/message_filters.py
+++ core/templatetags/message_filters.py
@@ -1,9 +1,22 @@
+# core/templatetags/message_filter.py
 from django import template
 
 register = template.Library()
 
-@register.filter
+@register.filter(name='get_item')
 def get_item(dictionary, key):
-    if dictionary is None:
-        return 0  # Or another default value
-    return dictionary.get(key, 0)
+   
+    if isinstance(dictionary, dict):
+        return dictionary.get(key)
+    return None 
+
+@register.filter(name='get_iterable_item')
+def get_iterable_item(dictionary, key):
+   
+    if isinstance(dictionary, dict):
+        # Return the item if it's a list/queryset, otherwise return empty list
+        value = dictionary.get(key)
+        # Check if the value is iterable but not a string (as looping over strings is usually not intended here)
+        if hasattr(value, '__iter__') and not isinstance(value, str):
+            return value
+    return [] 
