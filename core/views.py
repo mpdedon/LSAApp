9ef7c4091282719d56aa46cb4f72f7c47361a50a
Mpdedon <umarsmukhtar@gmail.com>
2026-01-13 22:36:04 +0100

    Align performance_ranges keys with logic and fix score ranges

--- core/views.py
+++ core/views.py
@@ -407,24 +407,25 @@ class AdminDashboardView(AdminRequiredMixin, TemplateView):
             
             if students_with_results:
                 performance_ranges = {
-                    'Excellent (90-100)': 0,
-                    'Very Good (80-89)': 0,
-                    'Good (70-79)': 0,
-                    'Average (60-69)': 0,
-                    'Below Average (<60)': 0
+                    'Excellent (80-100)': 0,
+                    'Very Good (70-79)': 0,
+                    'Good (60-69)': 0,
+                    'Average (50-59)': 0,
+                    'Below Average (<50)': 0
                 }
                 
                 for score in students_with_results:
-                    if score >= 90:
-                        performance_ranges['Excellent (90-100)'] += 1
-                    elif score >= 80:
-                        performance_ranges['Very Good (80-89)'] += 1
-                    elif score >= 70:
-                        performance_ranges['Good (70-79)'] += 1
-                    elif score >= 60:
-                        performance_ranges['Average (60-69)'] += 1
-                    else:
-                        performance_ranges['Below Average (<60)'] += 1
+                    if score is not None:
+                        if score >= 80:
+                            performance_ranges['Excellent (80-100)'] += 1
+                        elif score >= 70:
+                            performance_ranges['Very Good (70-79)'] += 1
+                        elif score >= 60:
+                            performance_ranges['Good (60-69)'] += 1
+                        elif score >= 50:
+                            performance_ranges['Average (50-59)'] += 1
+                        else:
+                            performance_ranges['Below Average (<50)'] += 1
                 
                 performance_labels = list(performance_ranges.keys())
                 performance_data = list(performance_ranges.values())
